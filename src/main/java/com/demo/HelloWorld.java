89bc9cbf326660a1c96a45f2d99a95befe61d485
kumar <kumar@example.com>
2026-03-14 22:59:09 +0530

    converted app to web server

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,11 +1,35 @@
 package com.demo;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) {
+                try {
+                    String response = "Hello from Jenkins CI Pipeline!";
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
-
 }
